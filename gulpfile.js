74ccaea20087e9132ee3810dbad2d9ca560947b5
jackfriend <jackwillamfriend@gmail.com>
2016-10-06 21:57:59 -0400

    started nav

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,16 @@ var rename  = require('gulp-rename');
 var browser = require('browser-sync').create();
 
 var paths = {
-    dest: '/dist',
-    static: this.dest + '/static',
-    pug: '/src/index.pug',
-    sass: '/src/sass/imports.sass'
+    dest: './dist',
+    static: './dist/static',
+    pug: './src/index.pug',
+    sass: './src/sass/imports.sass'
 }
 
 gulp.task('browser', function() {
     browser.init({
         server: {
-            baseDir: "/dist"
+            baseDir: "./dist"
         }
     });
 });
@@ -22,6 +22,7 @@ gulp.task('browser', function() {
 gulp.task('pug', function () {
     return gulp.src(paths.pug)
     .pipe(pug())
+    .pipe(rename('/index.html'))
     .pipe(gulp.dest(paths.dest));
 });
 
@@ -32,8 +33,8 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(paths.static));
 });
 
-gulp.task('watch', ['pug', 'sass', 'browser'], function () {
+gulp.task('default', ['pug', 'sass', 'browser'], function () {
     gulp.watch('src/sass/**/*.*', ['sass']);
     gulp.watch('src/**/*.pug', ['pug']);
-    gulp.watch('dist').on('change', browser.reload);
+    gulp.watch('dist/**/*.*').on('change', browser.reload);
 });
