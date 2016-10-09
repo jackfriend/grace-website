0f0d85466b2a190e00152f260b2e78f1b47d7323
jackfriend <jackwillamfriend@gmail.com>
2016-10-09 18:25:24 -0400

    v-for on nav and started carousel

--- README.md
+++ README.md
@@ -16,3 +16,7 @@
 ## Color
 - yellow: #ffeb3b or #FEC606
 - black: #3f3f3f or #282830 or #14141c
+
+## add to gh-pages
+git add dist && git commit -m "Initial dist subtree commit"
+git subtree push --prefix dist origin gh-pages
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,16 @@
-var gulp    = require('gulp');
-var pug     = require('gulp-pug');
-var sass    = require('gulp-sass');
-var rename  = require('gulp-rename');
-var browser = require('browser-sync').create();
+var gulp      = require('gulp');
+var pug       = require('gulp-pug');
+var sass      = require('gulp-sass');
+var rename    = require('gulp-rename');
+var prettify  = require('gulp-html-prettify');
+var browser   = require('browser-sync').create();
 
 var paths = {
     dest: './dist',
     static: './dist/static',
     pug: './src/index.pug',
-    sass: './src/sass/imports.sass'
+    sass: './src/sass/imports.sass',
+    js: './src/js/script.js'
 }
 
 gulp.task('browser', function() {
@@ -22,6 +24,7 @@ gulp.task('browser', function() {
 gulp.task('pug', function () {
     return gulp.src(paths.pug)
     .pipe(pug())
+    .pipe(prettify({indent_char: ' ', indent_size: 2}))
     .pipe(rename('/index.html'))
     .pipe(gulp.dest(paths.dest));
 });
@@ -33,8 +36,14 @@ gulp.task('sass', function () {
     .pipe(gulp.dest(paths.static));
 });
 
+gulp.task('js', function () {
+    return gulp.src(paths.js)
+    .pipe(gulp.dest(paths.static));
+});
+
 gulp.task('default', ['pug', 'sass', 'browser'], function () {
     gulp.watch('src/sass/**/*.*', ['sass']);
     gulp.watch('src/**/*.pug', ['pug']);
+    gulp.watch('src/js/script.js', ['js']);
     gulp.watch('dist/**/*.*').on('change', browser.reload);
 });
